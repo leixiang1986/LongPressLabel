cc1ab74745191fe7ec714829638c47e270a74387
leixiang <541150547@qq.com>
2017-10-10 11:42:24 +0800

    Initial Commit

---
+++ LongPressLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LongPressLabel
+//
+//  Created by 雷祥 on 2017/10/10.
+//  Copyright © 2017年 okdeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LongPressLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LongPressLabel
+//
+//  Created by 雷祥 on 2017/10/10.
+//  Copyright © 2017年 okdeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
